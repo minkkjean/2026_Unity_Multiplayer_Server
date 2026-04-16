54f0d804371e09a0de30fd8b5e7486ad9f36a42b
minkkjean <mkkjin@g.shingu.ac.kr>
2026-04-16 18:17:18 +0900

    [Feat] 3인칭 카메라 & 물건 상호작용

--- Assets/Scripts/FusionBootstrap.cs
+++ Assets/Scripts/FusionBootstrap.cs
@@ -15,6 +15,12 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
     [SerializeField] private NetworkPrefabRef playerPrefab;                 //네트워크에 등록된 프리팹
     [SerializeField] private Transform[] spawnPoints;                       //스폰 위치 설정
 
+    [Header("Pickable Box")]
+    [SerializeField] private NetworkPrefabRef pickableBoxPrefab;
+    [SerializeField] private Transform[] boxSpawnPoints;
+
+    private bool boxesSpawned =false;
+
     private Dictionary<PlayerRef, NetworkObject> playerObjects = new();
 
     private NetworkRunner runner;
@@ -22,13 +28,15 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
     public struct NetworkInputData : INetworkInput
     {
         public Vector2 move;
+        public float cameraYaw;
         public NetworkButtons buttons;
     }
 
     public enum InputButton
     {
         Fire = 0,
-        Jump = 1
+        Jump = 1,
+        Pickup = 2
     }
 
     public void StartHost() => _ = StartGame(GameMode.Host);
@@ -62,9 +70,39 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
         });
 
         if (result.Ok)
+        {
             Debug.Log($"[Fusion] StartGame OK - {mode} / {sessionName}");
+
+            if (runner.IsServer)
+            {
+                SpawnBoxes();
+            }
+        }
         else
+        {
             Debug.LogError($"[Fusion] StartGame FAILED - {result.ShutdownReason}");
+        }
+           
+    }
+
+    public void SpawnBoxes()
+    {
+        if (!runner.IsPlayer) return;
+
+        if (boxesSpawned) return;
+
+        boxesSpawned = true;
+
+        if (boxSpawnPoints == null || boxSpawnPoints.Length == 0) return;
+
+        foreach (var point in boxSpawnPoints)
+        {
+            if (point == null) continue;
+
+            runner.Spawn(pickableBoxPrefab, point.position, point.rotation, null);
+        }
+
+        Debug.Log($"상자 {boxSpawnPoints.Length} 개 생성 완료");
     }
 
 
@@ -87,6 +125,7 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
         );
 
         playerObjects[player] = obj;
+        runner.SetPlayerObject(player, obj);
     }
 
     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) { }
@@ -100,9 +139,12 @@ public class FusionBootstrap : MonoBehaviour, INetworkRunnerCallbacks
             Input.GetAxisRaw("Vertical")
         );
 
+        data.cameraYaw = SimplePlayer.LocalCameraYaw;
+
         var buttons =  new NetworkButtons();                                            //네트워크 버튼 생성
         buttons.Set((int)InputButton.Fire , Input.GetMouseButton(0));                   //마우스 버튼 
         buttons.Set((int)InputButton.Jump, Input.GetKey(KeyCode.Space));                //점프버튼
+        buttons.Set((int)InputButton.Pickup, Input.GetKey(KeyCode.E));                  //물건 상호작용 버튼
 
         data.buttons = buttons;
 
--- Assets/Scripts/SimplePlayer.cs
+++ Assets/Scripts/SimplePlayer.cs
@@ -1,4 +1,5 @@
 using Fusion;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SimplePlayer : NetworkBehaviour
@@ -37,7 +38,35 @@ public class SimplePlayer : NetworkBehaviour
     private int _lastJumpVisualTick = -1;
 
     [SerializeField] private GameObject cameraRoot;
+
+    private Transform cameraRootTransform;
+
+    private Camera localCamera;
     private Transform cameraTransform;
+
+    public static float LocalCameraYaw { get; private set; }
+
+    [Header("Camera")]
+    [SerializeField] private Vector3 cameraFollowOffset = new Vector3(0, 1.5f, 0f);
+    [SerializeField] private float cameraSenesitivity = 3.0f;
+    [SerializeField] private float minPitch = -30f;
+    [SerializeField] private float maxPitch = 60f;
+
+    private float cameraYaw;
+    private float cameraPitch = 15f;
+
+    [Header("Pickup")]
+                                            
+    [SerializeField] private Transform holdPoint;
+    [SerializeField] private float pickupDistance = 3.0f;
+    [SerializeField] private float dropForce = 2.0f;
+    [SerializeField] private LayerMask pickupMask;
+
+    [Networked] private NetworkObject HeldBox { get; set; }
+
+    public Vector3 HoldPointPosition =>
+        holdPoint != null ? holdPoint.position : transform.position + transform.forward * 1.2f + Vector3.up * 1.2f;
+
 
     public override void Spawned()
     {
@@ -48,11 +77,20 @@ public class SimplePlayer : NetworkBehaviour
 
         if (isMine)
         {
-            Camera cam = cameraRoot.GetComponentInChildren<Camera>(true);
-            if (cam != null)
+            cameraRootTransform = cameraRoot.transform;
+
+            localCamera = cameraRoot.GetComponentInChildren<Camera>(true);
+            if (localCamera != null)
             {
-                cameraTransform = cam.transform;
+                cameraTransform = localCamera.transform;
             }
+
+            cameraYaw = transform.eulerAngles.y;
+            cameraPitch = 15;
+            LocalCameraYaw = cameraYaw;
+
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
     }
 
@@ -60,23 +98,12 @@ public class SimplePlayer : NetworkBehaviour
     {
         if (GetInput<FusionBootstrap.NetworkInputData>(out var inputData))
         {
-            Vector3 move;
+            Quaternion camYawRotation = Quaternion.Euler(0.0f, inputData.cameraYaw, 0.0f);
 
-            if (Object.HasInputAuthority && cameraTransform != null)
-            {
-                Vector3 forward = cameraTransform.forward;
-                forward.y = 0f;
-                forward.Normalize();
+            Vector3 forward = camYawRotation * Vector3.forward;
+            Vector3 right = camYawRotation * Vector3.right;
 
-                Vector3 right = Vector3.Cross(forward, Vector3.up).normalized;
-
-                move = forward * inputData.move.y - right * inputData.move.x;
-            }
-            else
-            {
-                move = new Vector3(inputData.move.x, 0.0f, inputData.move.y);
-
-            }
+            Vector3 move = forward * inputData.move.y + right * inputData.move.x;
 
             if (move.sqrMagnitude > 1f)
                 move.Normalize();
@@ -93,9 +120,9 @@ public class SimplePlayer : NetworkBehaviour
             IsGroundedNet = grounded;
 
             //바닥에 붙어있을때 처리
-            if(grounded && VerticalVelocity < 0.0f)
+            if (grounded && VerticalVelocity < 0.0f)
             {
-                VerticalVelocity = 0f;
+                VerticalVelocity = -2f;
             }
 
             //점프
@@ -111,7 +138,7 @@ public class SimplePlayer : NetworkBehaviour
             VerticalVelocity += gravity * Runner.DeltaTime;
 
             //이동 분리
-            Vector3 horizontalMove = new Vector3(move.x * moveSpeed , 0f, move.z * moveSpeed);
+            Vector3 horizontalMove = new Vector3(move.x * moveSpeed, 0f, move.z * moveSpeed);
             transform.position += horizontalMove * Runner.DeltaTime;
 
             if (!(grounded && VerticalVelocity <= 0f))
@@ -119,10 +146,6 @@ public class SimplePlayer : NetworkBehaviour
                 Vector3 verticalMove = new Vector3(0f, VerticalVelocity, 0f);
                 transform.position += verticalMove * Runner.DeltaTime;
             }
-
-            PreviousButtons = inputData.buttons;    
-
-
 
             if (move.sqrMagnitude > 0.001f)
             {
@@ -136,7 +159,13 @@ public class SimplePlayer : NetworkBehaviour
             }
         }
 
-        //발사
+        if (inputData.buttons.WasPressed(PreviousButtons, (int)FusionBootstrap.InputButton.Pickup))
+        {
+            if (!TryDropHeldBox())
+                TryPickupBox();
+        }
+
+        //발사    
         if (inputData.buttons.IsSet((int)FusionBootstrap.InputButton.Fire))
         {
             if (FireCooldown.ExpiredOrNotRunning(Runner))
@@ -146,6 +175,9 @@ public class SimplePlayer : NetworkBehaviour
             }
 
         }
+
+        PreviousButtons = inputData.buttons;
+
     }
 
     private void Fire()
@@ -223,4 +255,63 @@ public class SimplePlayer : NetworkBehaviour
         animator.SetBool("FreeFall", !IsGroundedNet && VerticalVelocity <= 0.1f);
         animator.SetFloat("MotionSpeed", 3f);
     }
+
+    private void LateUpdate()
+    {
+        if (!Object || !Object.HasInputAuthority || cameraRoot == null || cameraTransform == null) return;
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        cameraYaw += mouseX * cameraSenesitivity;
+        cameraPitch -= mouseY * cameraSenesitivity;
+        cameraPitch = Mathf.Clamp(cameraPitch, minPitch, maxPitch);
+
+        //yaw는 루트가 담당
+        cameraRoot.transform.localRotation = Quaternion.Euler(0.0f, cameraYaw - transform.eulerAngles.y, 0f);
+
+        //pitch는 실제 카메라가 담당
+        cameraTransform.localRotation = Quaternion.Euler(cameraPitch, 0.0f, 0.0f);
+
+        LocalCameraYaw = cameraTransform.eulerAngles.y;
+
+
+    }
+
+    void TryPickupBox()
+    {
+        if (!Object.HasStateAuthority) return;
+
+        if (HeldBox != null) return;
+
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.forward;
+
+        Debug.DrawRay(origin, direction * pickupDistance, Color.red, 3f);
+
+        if (Physics.Raycast(origin, direction, out RaycastHit hit, pickupDistance, pickupMask))
+        {
+            PickupBox box = hit.collider.GetComponentInChildren<PickupBox>();
+            if (box == null) return;
+
+            box.PickUp(Object.InputAuthority);  
+            HeldBox = box.Object;
+        }
+    }
+
+    private bool TryDropHeldBox()
+    {
+        if (!Object.HasStateAuthority)
+            return false;
+
+        if (HeldBox == null)
+            return false;
+
+        PickupBox box = HeldBox.GetComponent<PickupBox>();
+        if (box == null) return false;
+
+        box.Drop(transform.forward * dropForce);
+        HeldBox = null;
+        return true;
+    }   
 }
